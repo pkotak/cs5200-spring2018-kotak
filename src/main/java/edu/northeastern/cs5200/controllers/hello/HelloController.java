06602bd8c09835ef6ec83ea71e4934e533a42c4a
Paarth Kotak <paarthkotak@gmail.com>
2018-01-12 21:08:43 -0500

    initial commit

---
+++ src/main/java/edu/northeastern/cs5200/controllers/hello/HelloController.java
@@ -0,0 +1,21 @@
+package edu.northeastern.cs5200.controllers.hello;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class HelloController {
+
+ @RequestMapping("/api/hello/string")
+ public String sayHello() {
+  return "Hello Paarth Kotak!";
+ }
+
+ @RequestMapping("/api/hello/object")
+ public HelloObject sayHelloObject() {
+  HelloObject obj = new HelloObject("Hello Paarth Kotak!");
+  return obj;
+ }
+
+}
